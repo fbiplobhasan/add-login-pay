72b4a73b0778bd0d31502b3af196463f4296f3bd
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-16 13:13:00 +0600

    add home interface

--- js/login.js
+++ js/login.js
@@ -5,5 +5,17 @@ document.getElementById('button-login').addEventListener('click', function (even
     // console.log('login button clicked');
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+
+
+    if (phoneNumber === '5' && pinNumber === '1234') {
+        console.log('You are origin');
+        window.location.href='/Payoo.html'
+
+    } else {
+        alert('Wrong Phone Number or Pin')
+    }
+    // console.log(phoneNumber,pinNumber);
+
+
 })
