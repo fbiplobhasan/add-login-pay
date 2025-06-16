e48d94754386209a47872ac71057683b8503c28a
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-16 12:40:07 +0600

    simple login form added.

---
+++ js/login.js
@@ -0,0 +1,9 @@
+// console.log('button file clicked added');
+
+document.getElementById('button-login').addEventListener('click', function (event) {
+    event.preventDefault();
+    // console.log('login button clicked');
+
+    const phoneNumber = document.getElementById('phone-number').value;
+    console.log(phoneNumber);
+})
