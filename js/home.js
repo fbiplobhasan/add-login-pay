5a75b7a583f63d29faa39522bca22b3b9f48715f
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-17 06:32:00 +0600

    add money some code

--- js/home.js
+++ js/home.js
@@ -1,1 +1,10 @@
-console.log('Home js');
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // step-2
+
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+})
