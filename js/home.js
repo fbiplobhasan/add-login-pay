51b55f46e9f4edc6ba2f2f071df990abd0b693a2
Biplob hasan <fbiplobhasan2024@gmail.com>
2025-06-17 06:50:17 +0600

    add with exsisting balance

--- js/home.js
+++ js/home.js
@@ -5,6 +5,28 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 
     const addMoneyInput = document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
+    const addMoneyAmount = parseFloat(addMoneyInput);
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(pinNumberInput);
+    // console.log(typeof addMoneyAmount);
+
+    // step-3
+
+    if (pinNumberInput === '1234') {
+
+        // step-4
+
+        const balance = document.getElementById('account-balance').innerText;
+        const existingBalance = parseFloat(balance);
+        // console.log(typeof existingBalance);
+
+        // step-5
+
+        const newBalance = existingBalance + addMoneyAmount;
+        // console.log(newBalance);
+
+        document.getElementById('account-balance').innerText = newBalance;
+
+    } else {
+        alert('Failed to add money! please try again...')
+    }
 })
